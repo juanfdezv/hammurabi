64649a8ee31297c2f92dfbf9c63ea0d278e12fef
Juan <juanfernandezv6@gmail.com>
2020-10-31 18:03:31 -0300

    Add City class

--- hammurabi.py
+++ hammurabi.py
@@ -1,31 +1,36 @@
 import shutil
 
+class City:
+    def __init__(self):
+        self.starved_people = 0
+        self.population = 100
+        self.acres = 1000
+        self.bushels_per_acre = 3
+        self.rat_food = 200
+        self.bushels_in_store = 2800
+        self.land_price = 26
+        self.acres_bought = 0
+    
+    
+
 def hammurabi():
-    
-    starved_people = 0
-    population = 100
-    acres = 1000
-    bushels_per_acre = 3
-    rat_food = 200
-    bushels_in_store = 2800
-    land_price = 26
-    
+    city = City()
     print_introduction()
     for year in range(1, 11):
-        print("Hamurabi: I beg to report to you,")
-        print("In year ", year, ", ", starved_people, " people starved")
-        print("Population is now ", population)
-        print("The city now owns ", acres, " acres")
-        print("You harvested ", bushels_per_acre, " bushels per acre")
-        print("Rats ate ", rat_food, " bushels")
-        print("You now have ", bushels_in_store, " in store")
-        print("\nLand is trading at ", land_price, " bushels per acre")
+        print("\nHamurabi: I beg to report to you,")
+        print("In year ", year, ", ", city.starved_people, " people starved")
+        print("Population is now ", city.population)
+        print("The city now owns ", city.acres, " acres")
+        print("You harvested ", city.bushels_per_acre, " bushels per acre")
+        print("Rats ate ", city.rat_food, " bushels")
+        print("You now have ", city.bushels_in_store, " in store")
+        print("\nLand is trading at ", city.land_price, " bushels per acre")
+
 
 def print_introduction():
     lines = ["Hamurabi", "Try your hand at governing ancient Sumeria", "for ten-year term of office"]
     for line in lines:
         print(line.center(shutil.get_terminal_size().columns))
-
 
     
 
